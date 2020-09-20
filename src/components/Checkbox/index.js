0835bd305b88113692075f0bb5adf64af72ed7d8
erretres <diegoperezcorreo@gmail.com>
2020-09-20 10:03:25 -0300

    Refactor, from class to function:  Checkbox.
    - useState to replace setState
    
    - Remove  proptypes
    
    - Keep handleCheckboxChange and toggleCheckboxChange:
        At the moment, the idea is just migrate from classes to functions

--- src/components/Checkbox/index.js
+++ src/components/Checkbox/index.js
@@ -1,45 +1,28 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 
-class Checkbox extends Component {
-  static propTypes = {
-    label: PropTypes.string.isRequired,
-    handleCheckboxChange: PropTypes.func.isRequired
-  };
+function Checkbox(props) {
+    var  [ isChecked, setIsChecked ]                            = useState(false);
+    const { handleCheckboxChange, label, classes }  = props; 
 
-  state = {
-    isChecked: false
-  };
+    const toggleCheckboxChange = label => {
+        setIsChecked(!isChecked);
+        handleCheckboxChange(label);
+    };
 
-  toggleCheckboxChange = () => {
-    const { handleCheckboxChange, label } = this.props;
-
-    this.setState(({ isChecked }) => ({
-      isChecked: !isChecked
-    }));
-
-    handleCheckboxChange(label);
-  };
-
-  render() {
-    const { label, classes } = this.props;
-    const { isChecked } = this.state;
-
-    return (
+     return (
       <div className={classes}>
         <label>
           <input
             type="checkbox"
             value={label}
             checked={isChecked}
-            onChange={this.toggleCheckboxChange}
+         onChange = {() =>  toggleCheckboxChange(label) }
           />
-
           <span className="checkmark">{label}</span>
         </label>
       </div>
-    );
-  }
+    ); 
+
 }
 
 export default Checkbox;
